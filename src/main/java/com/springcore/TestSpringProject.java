9d6c61325609c6840de42267c770f05495889590
Lakshay Gupta <lakshay.gupta@timesinternet.in>
2021-08-12 12:41:25 +0530

    Added menu

--- src/main/java/com/springcore/TestSpringProject.java
+++ src/main/java/com/springcore/TestSpringProject.java
@@ -9,17 +9,18 @@ public class TestSpringProject {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
 		context.scan("com.springcore");
 		context.refresh();
-		Main obj = (Main) context.getBean("Main");
-		System.out.println("Select option: 1. Add 2: Subtract 3. Multiply 4. Divide");
-		Scanner sc = new Scanner(System.in);
-		int option = sc.nextInt();
-		System.out.println("Enter two values");
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		
-		
-		System.out.println(obj.calculateOp(a, b, option));
+		while(true) {
+			Main obj = (Main) context.getBean("Main");
+			System.out.println("Select option: 1. Add 2: Subtract 3. Multiply 4. Divide");
+			Scanner sc = new Scanner(System.in);
+			int option = sc.nextInt();
+			System.out.println("Enter two values");
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(obj.calculateOp(a, b, option));
 
-		context.close();
+		}
+
+		//context.close();
 	}
 }
